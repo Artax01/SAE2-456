d1a668edb013ccef4bc6e1de0bb550265f639fec
Maxence Marie <maxence.marie@etu.unicaen.fr>
2025-06-08 23:32:04 +0200

    Ajout des balises meta pour le SEO et la compatibilité + meilleur importation de tailwindcss

--- web/index.php
+++ web/index.php
@@ -4,26 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- 
-        S'il vous plaît ne supprimez pas ces balises script:
-        elles servent à utliser TailwindCSS ainsi que Preline, 
-        deux frameworks complémentaire.
-    -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://unpkg.com/preline@latest/dist/preline.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>RAPIDC3</title>
+
+    <!-- SEO (référencement) -->
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="RapidC3">
+    <meta name="language" content="fr">
+    <meta name="keywords" content="restaurant, vente à emporter, RapidC3, plats, menus, fidélité">
+    <meta name="description" content="RapidC3 vous permet de commander en ligne, de gérer vos points de fidélité et de profiter de plats délicieux à récupérer en magasin." inertia="description">
+    <meta name="twitter:card" content="summary_large_image" inertia="twitter:card">
+    <meta name="twitter:site" content="@rapidc3" inertia="twitter:site">
+    <meta name="twitter:title" content="RapidC3 - Commande en ligne et fidélité client" inertia="twitter:title">
+    <meta name="twitter:description" content="Commandez vos plats préférés, gagnez des points et profitez d'offres exclusives avec RapidC3, la solution en ligne pour la vente à emporter." inertia="twitter:description">
+    <meta name="twitter:creator" content="@rapidc3" inertia="twitter:creator">
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:type" content="website" inertia="og:type">
+    <meta property="og:site_name" content="RapidC3">
+    <meta property="og:title" content="RapidC3 - Plats à emporter et programme fidélité" inertia="og:title">
+    <meta property="og:description" content="Découvrez RapidC3 : restaurants à emporter avec commandes en ligne, points de fidélité et promotions personnalisées." inertia="og:description">
+
+    <!-- S'il vous plaît ne supprimez pas cette balise script: elle sert à utliser TailWindCSS.-->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
 
 
     <?php
-        echo "<h2>Bienvenue sur l'application web de la SAE2-456</h2>"."</br>";
+        echo "<h2>Bienvenue sur l'application web de la SAE2-456 pour le client RAPIDC3.</h2>"."</br>";
     ?>
 
 
-    <script>
-        window.addEventListener('load', () => {
-            window.HSStaticMethods.autoInit();
-        });
-    </script>
 </body>
 </html>
