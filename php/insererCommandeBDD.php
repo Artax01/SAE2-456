820ebd320a68bc92774d314c3cd09d4e099b38ba
Zoe Margerie <margeri231@campus.unicaen.fr>
2025-06-11 18:25:26 +0200

    enregistrer un plat

--- php/insererCommandeBDD.php
+++ php/insererCommandeBDD.php
@@ -15,28 +15,35 @@ function récupérerUnPlat($conn){
 
 
 function enregistrerUneCommande($conn, $platNum, $resNum, $dateRecup){
-    if(isset($_post['bouton_payer'])){
-        $platNum= $_post['bouton'];
-    }
-    $sql3=$conn->prepare("SELECT * FROM RAP_PLAT where pla_num = $platNum;");
-    $sql3->execute();
-    $res = $sql3->fetchAll(PDO::FETCH_ASSOC);
-    $prixHT = $res["pla_prix_vente_unit_ht"];
-    $dureeTotal=$res["pla_duree_preparation"];
-    $tva=$res["pla_tva"];
-    $prixTotal=$prixHT*$tva;
-    $promo=$res["pla_promotion"];
-    $nbPoints= $res["pla_nb_points"];
-    
+   
+   $sql3="SELECT * FROM RAP_PLAT where pla_num= '$platNum'";
+   $cur3=preparerRequetePDO($conn, $sql3);
+   $tab = array();
+   LireDonneesPDOPreparee($cur3,$tab);
+   var_dump($tab);
 
-
-    $sql="INSERT INTO rap_commande (res_num, com_num,  cli_num, com_date, com_heure_recup, com_prix_total, com_reduc_points, com_reduc_promo, com_duree_totale_prepa)VALUES ($resNum, (select Max(com_num)+1 from rap_commande), '124', SYSDATE, TO_DATE($dateRecup, 'YYYY-MM-DD HH24:MI:SS'), $prixTotal, $, 0, 15);";
-    $sql2="INSERT INTO rap_appartenir(res_num, com_num, pla_num, app_quantite) values ($resNum, (select Max(com_num)+1 from rap_commande), $platNum, 1)";
+   $prixHT = (double)$tab[0]["PLA_PRIX_VENTE_UNIT_HT"];
+   $dureeTotal=$tab[0]["PLA_DUREE_PREPARATION"];
+    $tva=(double)$tab[0]["PLA_TVA"];
+    $prixTotal=$prixHT+($prixHT*($tva/100));
+    $promo=$tab[0]["PLA_PROMOTION"];
+    $nbPoints= $tab[0]["PLA_NB_POINTS"];
+    var_dump($promo);
+    var_dump($prixHT);
+    print_r($prixHT);
+    var_dump($dureeTotal);
+    var_dump($prixTotal);
+    var_dump($tva);
+   $sql="INSERT INTO rap_commande (res_num, com_num,  cli_num, com_date, com_heure_recup, com_prix_total, com_reduc_points, com_reduc_promo, com_duree_totale_prepa)VALUES ($resNum, (SELECT MAX(com_num) + 1 FROM rap_commande), '124', SYSDATE, TO_DATE('$dateRecup','hh24:mi:ss'), $prixTotal, $promo, $nbPoints, $dureeTotal)";
+   $sql2="INSERT INTO rap_appartenir(res_num, com_num, pla_num, app_quantite) values ($resNum, (select Max(com_num)+1 from rap_appartenir), $platNum, 1)";
     $cur =preparerRequetePDO($conn, $sql);
-    $cur2 =preparerRequetePDO($conn, $sql);
+    $cur2 =preparerRequetePDO($conn, $sql2);
     majDonneesPrepareesPDO($cur);
     majDonneesPrepareesPDO($cur2);
+    
 
 }   
+
+enregistrerUneCommande($conn, 100, 1, '20:50:00');
 
 ?>
