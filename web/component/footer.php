87c722665e4233e4bdddc5f0f3c6c98739b9ab27
Charly ANTOINE <charly.antoine@etu-unicaen.fr>
2025-06-11 14:09:39 +0200

    Fin du footer, ajout logo + separateur

--- web/component/footer.php
+++ web/component/footer.php
@@ -1,38 +1,42 @@
-<footer class="bg-white rounded-lg shadow sm:flex sm:items-center sm:justify-between p-4 sm:p-6 xl:p-8 dark:bg-gray-800 antialiased">
-  <p class="mb-4 text-sm text-center text-gray-500 dark:text-gray-400 sm:mb-0">
-      &copy; 2025 RAPIDC3   Tout droits réservés.
-  </p>
-  <div class="flex justify-center items-center space-x-1">
-    <a href="#" data-tooltip-target="tooltip-facebook" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
+<footer class="relative bg-white rounded-lg shadow p-6 dark:bg-gray-800 antialiased border-t border-gray-300 dark:border-gray-600">
+  <!-- Logo centré -->
+  <div class="absolute inset-0 flex justify-center items-center pointer-events-none z-0">
+    <img src="img/rapidc3-logo.png" alt="Logo C3" class="w-20 h-20">
+  </div>
+
+  <!-- Contenu principal : copyright à gauche, icônes à droite -->
+  <div class="relative z-10 flex justify-between items-center">
+    <!-- Copyright -->
+    <p class="text-sm text-gray-500 dark:text-gray-400">
+      &copy; 2025 RAPIDC3. Tous droits réservés.
+    </p>
+
+    <!-- Icônes réseaux sociaux -->
+    <div class="flex space-x-3">
+      <!-- Facebook -->
+      <a href="#" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-            <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
+          <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
         </svg>
         <span class="sr-only">Facebook</span>
-    </a>
-    <div id="tooltip-facebook" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-        Like us on Facebook
-        <div class="tooltip-arrow" data-popper-arrow></div>
-    </div>
-    <a href="#" data-tooltip-target="tooltip-twitter" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
+      </a>
+
+      <!-- Twitter -->
+      <a href="#" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path fill="currentColor" d="M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z"/>
+          <path fill="currentColor" d="M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z"/>
         </svg>
         <span class="sr-only">Twitter</span>
-    </a>
-    <div id="tooltip-twitter" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-        Follow us on Twitter
-        <div class="tooltip-arrow" data-popper-arrow></div>
-    </div>
-    <a href="#" data-tooltip-target="tooltip-instagram" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
-    <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
-        <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9S160.5 370.8 224.1 370.8 339 319.5 339 255.9 287.7 141 224.1 141zm0 186.6c-39.6 0-71.8-32.2-71.8-71.8s32.2-71.8 71.8-71.8 71.8 32.2 71.8 71.8-32.2 71.8-71.8 71.8zm146.4-194.3c0 14.9-12.1 27-27 27-14.9 0-27-12.1-27-27s12.1-27 27-27 27 12.1 27 27zm76.1 27.2c-.6-35.3-9.9-66.7-36.2-93S360.3.6 325 .1C289.3-.4 158.7-.4 123 .1 87.7.6 56.3 9.9 30 36.2S.6 87.7.1 123C-.4 158.7-.4 289.3.1 325c.5 35.3 9.8 66.7 36.1 93s57.7 35.6 93 36.1c35.7.5 166.3.5 202 0 35.3-.5 66.7-9.8 93-36.1s35.6-57.7 36.1-93c.5-35.7.5-166.3 0-202zM398.8 388c-7.8 19.6-22.9 34.7-42.5 42.5-29.4 11.7-99.2 9-132.3 9s-102.9 2.6-132.3-9c-19.6-7.8-34.7-22.9-42.5-42.5-11.7-29.4-9-99.2-9-132.3s-2.6-102.9 9-132.3c7.8-19.6 22.9-34.7 42.5-42.5 29.4-11.7 99.2-9 132.3-9s102.9-2.6 132.3 9c19.6 7.8 34.7 22.9 42.5 42.5 11.7 29.4 9 99.2 9 132.3s2.7 102.9-9 132.3z"/>
-    </svg>
-    <span class="sr-only">Instagram</span>
-</a>
-<div id="tooltip-instagram" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
-    Follow us on Instagram
-    <div class="tooltip-arrow" data-popper-arrow></div>
-</div>
+      </a>
 
-</div>
+      <!-- Instagram -->
+      <a href="#" aria-label="Instagram" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 hover:text-gray-900 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600">
+        <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.75 2A5.75 5.75 0 0 0 2 7.75v8.5A5.75 5.75 0 0 0 7.75 22h8.5A5.75 5.75 0 0 0 22 16.25v-8.5A5.75 5.75 0 0 0 16.25 2h-8.5ZM12 7.25A4.75 4.75 0 1 1 7.25 12 4.75 4.75 0 0 1 12 7.25Zm5.5-1.5a1.25 1.25 0 1 1-1.25 1.25A1.25 1.25 0 0 1 17.5 5.75ZM12 9a3 3 0 1 0 3 3 3 3 0 0 0-3-3Z"/>
+        </svg>
+    </a>
+
+
+    </div>
+  </div>
 </footer>
