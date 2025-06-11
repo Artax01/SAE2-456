503256be20f09ace513a3059c35110b91d3e31a4
Zoe Margerie <margeri231@campus.unicaen.fr>
2025-06-11 11:09:40 +0200

    connexion php

---
+++ php/connexion.php
@@ -0,0 +1,19 @@
+
+<?php
+include("pdo_agile.php");
+include("param_connexion_etu.php");
+$db_username = $db_usernameOracle;		
+$db_password = $db_passwordOracle;
+$db = $dbOracle;
+
+$conn = OuvrirConnexionPDO($db,$db_username,$db_password);
+
+if ($conn)
+	{
+		echo (" Connexion réussie à la base de données <br/>");
+		
+	}
+	else
+		echo ("Connexion impossible à la base de données <br/>");
+
+?>
