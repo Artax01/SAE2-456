e4c4ee9f0fa8f6619b61f674f7b61f3afb6b4482
Nathan Elie <elie243@campus.unicaen.fr>
2025-06-11 11:44:28 +0200

    modification des pages acceuil et navbar

--- web/page/accueil.php
+++ web/page/accueil.php
@@ -1,2 +1,9 @@
-<h2>Accueil</h2>
-<p>Bienvenue sur la page d'accueil de l'application mobile.</p>
+<div class="relative w-full h-screen bg-[url('img/rapidc3.png')] bg-cover bg-center bg-no-repeat">
+    <div class="absolute inset-0 bg-white opacity-80 z-0"></div>
+    <div class="absolute inset-0 flex flex-col items-center justify-center z-10">
+        <h1 class="text-7xl font-extrabold tracking-tight mb-4 text-black drop-shadow-lg">RAPID C3</h1>
+        <p class="text-2xl font-semibold text-black text-center max-w-2xl drop-shadow-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a sagittis leo. Aliquam ut nibh ac massa scelerisque pretium non id nisi. Quisque sodales, urna eu tempor mollis, diam nisl porttitor tellus, vitae tempus libero nisi non nulla. Aliquam erat volutpat.
+        </p>
+    </div>
+</div>
