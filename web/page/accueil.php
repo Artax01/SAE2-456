f2601c0b63ea4f583b346a59e2b544cbf4ef24c1
elie243 <nathan.elie@etu.unicaen.fr>
2025-06-11 14:36:01 +0200

    ajout plat.php

--- web/page/accueil.php
+++ web/page/accueil.php
@@ -22,7 +22,7 @@
                 </svg>
                 <span class="mt-1 text-xs font-semibold text-gray-700">Menu</span>
             </a>
-            <a href="?page=panier.php" class="flex flex-col items-center group">
+            <a href="?page=plat.php" class="flex flex-col items-center group">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-orange-400 group-hover:text-orange-500 transition" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.35 2.7A1 1 0 007 17h10a1 1 0 00.95-.68L21 13M7 13V6a1 1 0 011-1h6a1 1 0 011 1v7" />
                 </svg>
