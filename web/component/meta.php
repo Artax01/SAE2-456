493179ff949e84ebb53df8784d1894395d686feb
marie2421 <maxence.marie@etu.unicaen.fr>
2025-06-12 14:59:05 +0200

    modification du titre des pages par RAPIDC3

--- web/component/meta.php
+++ web/component/meta.php
@@ -1,5 +1,5 @@
 <?php
-  $titre = "RAPIDC3";
+  $_SESSION['title'] = 'RAPIDC3'
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Se connecter</title>
+  <title><?php echo $_SESSION['title']; ?></title>
 
   <!-- SEO (référencement) -->
   <meta name="robots" content="index, follow">
