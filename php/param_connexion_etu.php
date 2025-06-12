78ec17df0aac43d14ef723bf2dc72833c68ad1f7
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-06-12 13:56:08 +0200

    param_conn

--- php/param_connexion_etu.php
+++ php/param_connexion_etu.php
@@ -3,10 +3,10 @@
 	// préparation SAE 2.456 : paramètres de connexion à un SGBD Oracle ou MySQL
 	// param_connexion_etu.php 29/05/2021
 
-	// $db_usernameOracle = "agile_2";
-	// $db_passwordOracle = "agile_2"; // 
-	// $dbOracle = "oci:dbname=harpagon.unicaen.fr:1521/info.harpagon.unicaen.fr;charset=AL32UTF8";  
-	$db_usernameOracle = "system";
-	$db_passwordOracle = "oracle";
-	$dbOracle = "oci:dbname=//localhost:1521/xe;charset=AL32UTF8";  
+	 $db_usernameOracle = "agile_2";
+	 $db_passwordOracle = "agile_2"; // 
+	 $dbOracle = "oci:dbname=harpagon.unicaen.fr:1521/info.harpagon.unicaen.fr;charset=AL32UTF8";  
+	//$db_usernameOracle = "system";
+	//$db_passwordOracle = "oracle";
+	//$dbOracle = "oci:dbname=//localhost:1521/xe;charset=AL32UTF8";  
  ?>
