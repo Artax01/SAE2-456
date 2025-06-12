9233b2ad5351d08dfb1f01b8810442f3c181dba1
Gabin Dupuy--Guillemette <dupuygu241@campus.unicaen.fr>
2025-06-12 10:30:42 +0200

    encore réglagle...

--- web/session/session.php
+++ web/session/session.php
@@ -11,10 +11,13 @@ function getClient() {
     return $_SESSION['client'] ?? null;
 }
 
+<<<<<<< HEAD
 function getId() {
     return $_SESSION['client']['id'] ?? null;
 }
 
+=======
+>>>>>>> menu
 function getNom() {
     return $_SESSION['client']['nom'] ?? null;
 }
@@ -23,10 +26,13 @@ function getPrenom() {
     return $_SESSION['client']['prenom'] ?? null;
 }
 
+<<<<<<< HEAD
 function getTel() {
     return $_SESSION['client']['tel'] ?? null;
 }
 
+=======
+>>>>>>> menu
 function getEmail() {
     return $_SESSION['client']['email'] ?? null;
 }
