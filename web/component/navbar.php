c530c40c4aeec03fb6fd79a45355f6b179f50a64
elie243 <nathan.elie@etu.unicaen.fr>
2025-06-12 17:34:19 +0200

    correction place logo et place boutons nav bar sup

--- web/component/navbar.php
+++ web/component/navbar.php
@@ -35,12 +35,10 @@ require_once './web/session/session.php';
 <body class="bg-neutral-900 text-white">
 <header id="top-navbar"
   class="sticky top-0 inset-x-0 z-50 w-full text-white">
-  <nav class="relative max-w-[85rem] w-full mx-auto flex items-center justify-between sm:px-6 lg:px-8 py-2">
+  <nav class="w-full flex items-center justify-between px-0 py-2"> <!-- retire mx-auto et max-w-[85rem] -->
 
-
-    
     <!-- logo centré sur mobile -->
-    <div class="w-full md:w-auto flex justify-center md:justify-start order-1 md:order-none">
+    <div class="flex items-center ml-4">
       <button id="btn-title" class="text-white font-semibold focus:outline-hidden focus:opacity-80" onclick="loadPage('accueil.php', this)">
         <img src="./web/assets/img/logoC3.png" class="h-20 w-23">
       </button>
@@ -50,9 +48,9 @@ require_once './web/session/session.php';
     <?php require_once './php/connexion.php'; ?>
 
     <!-- buttons -->
-    <div id="hs-header-scrollspy" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-header-scrollspy-collapse">
-      <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-        <div data-hs-scrollspy="#scrollspy" class="text-xl py-2 md:py-0 [--scrollspy-offset:220] md:[--scrollspy-offset:70] flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
+  <div id="hs-header-scrollspy" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-header-scrollspy-collapse">
+   <div class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+    <div data-hs-scrollspy="#scrollspy" class="text-xl py-2 md:py-0 [--scrollspy-offset:220] md:[--scrollspy-offset:70] flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1 mr-4">
         <?php if (!isLoggedIn()): ?>
             <button id="btn-signup" onclick="loadPage('signup.php', this)"
               class="py-2 px-6 flex items-center text-white hover:bg-orange-600 !bg-orange-400 !hover:bg-orange-600 rounded-full">
@@ -85,7 +83,6 @@ require_once './web/session/session.php';
       </div>
     </div>
     <!-- end buttons -->
-
   </nav>
 </header>
 <!-- ========== END NAVBAR DESKTOP ========== -->
