0c4df34a45ec26612af889d59b6eb7f08bd565e5
Titouan Macre <macre241@campus.unicaen.fr>
2025-06-12 17:36:43 +0200

    Suppression des identifiants de test de connexion

--- web/page/signin.php
+++ web/page/signin.php
@@ -18,12 +18,12 @@ else {
 <?php require_once '../component/CASBanner.php';?>
 
 <div id="box-container" class="bg-black">
-  <div id="div-signin" class="mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs ">
+  <div id="div-signin" class="mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs">
     <div class="p-4 sm:p-7">
       <div class="text-center">
         <h1 class="block text-2xl font-bold text-gray-800">
           Se connecter
-      </h1>
+        </h1>
         <p class="mt-2 text-sm text-gray-600">
           Vous n'avez pas encore de compte ?
           <a href="./signup.php" class="text-blue-600 decoration-2 hover:underline focus:outline-hidden focus:underline font-medium dark:text-blue-500">
@@ -115,9 +115,3 @@ else {
     </div>
   </div>
 </div>
-
-<p>test@gmail.com</p>
-<p>123testABC</p>
-
-<p>perno@rso.fr</p>
-<p>LEPERENOEL</p>
